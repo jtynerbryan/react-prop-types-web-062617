06f6444089482cf94530f7d5ae2ad31982a362dc
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-08-28 16:25:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
+ 
+class Order extends React.Component {
+ 
+  render() {
+    return //order
+    	
+
+
+
+  }
+}
+ 
+Order.defaultProps = {
+  cone: true,
+  size: 'regular'
+};
+ 
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
+
